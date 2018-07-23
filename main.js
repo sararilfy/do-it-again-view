7daac361a9c47ab2589e186e7a885feb6f14f646
nakayama_yoshie <nakayama_yoshie@aratana.jp>
2018-07-23 09:35:41 +0900

    Method Event Handlers

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'test',
-    scroll: 0
+    count: 0
   },
-  mounted: function() {
-    this.scroll = 100
+  methods: {
+    increment: function() {
+      this.count += 1;
+    }
   }
 });
