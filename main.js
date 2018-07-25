025ba80b7fabe275e893b05ee55efbd29ce7249f
nakayama_yoshie <nakayama_yoshie@aratana.jp>
2018-07-25 09:34:59 +0900

    Dynamic State Transitions

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-    item: {
-      id: 1,
-      src: 'item1.jpg',
-      alt: 'サムネイル',
-      width: 200,
-      height: 200
-    }
+    radius: 50
   }
 });
