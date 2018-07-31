734231b70b9e2911ef3947998d17e6dbc5004819
nakayama_yoshie <nakayama_yoshie@aratana.jp>
2018-07-31 09:16:02 +0900

    List Rendering02 v-for and v-if

---
+++ 10/10-2.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    monster: [
+      { id: 1, name: 'ぶい', hp:100 },
+      { id: 2, name: 'しゃわー', hp:400 },
+      { id: 3, name: 'ブラッキ', hp:500 }
+    ]
+  }
+});
